bdca08621bae94368450a07353eb4ade20e04bcd
Nikesh P Bawankar <124368328+NikeshBawankar1921@users.noreply.github.com>
2025-06-11 19:06:52 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
           transition={{ duration: 0.8 }}
         >
           <div className='flex   bg-gradient-to-r via-gray-400 opacity-70  '><img className="justify-center  rounded-lg  w-50  rotate-y-180 pt-3 ml-2" src="https://i.ibb.co/4hHSNHC/1749634840250.png" />
-          <div className="  bg-clip-text w-full relative"> <h1 className='text-4xl md:text-5xl font-bold text-cyan-300 mt-25'>Nikesh Bawankar</h1>
+          <div className="  bg-clip-text w-full relative"> <h1 className='text-2xl md:text-5xl font-bold text-cyan-300 mt-25'>Nikesh Bawankar</h1>
           <div className='justify-center align-middle text-white md:text-2xl'>Full Stack and Embedded Software Developer</div></div>
           </div>
 
